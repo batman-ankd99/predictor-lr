0cb8a980cb3ff34540957668d92f86e10bfce255
ankur <ankd99@gmail.com>
2026-04-12 18:19:10 +0530

    starting for linear regression file

--- lr.py
+++ lr.py
@@ -2,3 +2,8 @@ class myLR:
     def __init__(self):
         self.m =  None
         self.b = None
+
+    def fit(self, train_x, train_y):
+        pass
+    def predict(self, x_test):
+        pass     
